53ad3d3b00dfd4c52d9b453f1222ed0ebbec1810
Parth Kalbag <10bparth@gmail.com>
2020-11-16 22:24:50 +0530

    View Customers Integrated With Backend

--- view_customers.php
+++ view_customers.php
@@ -76,23 +76,34 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>12345</td>
-                            <td>Parth Kalbag</td>
-                            <td>abc@gmail.com</td>
-                            <td>987654321</td>
-                            <td>85000</td>
-                            <td style="display: flex; justify-content: center">
-                                <a href="#" onclick="openCustomerModal()">
-                                    <div>
-                                        <span>View Data</span>
-                                    </div>
-                                    <div>
-                                        <img src="images/external-link-alt-solid.png" alt="" style="width: 20px; height: 20px">
-                                    </div>
-                                </a>
-                            </td>
-                        </tr>
+                        <?php
+                            require_once "mysql_connection.php";
+
+                            $sql = "SELECT * FROM users";
+                            $customerDetails = $conn->query($sql);
+
+                            if($customerDetails->num_rows > 0) {
+                                while($customerRow = $customerDetails->fetch_assoc()) {
+                                    echo "<tr>";
+                                    echo "<td>" . $customerRow["user_id"] . "</td>";
+                                    echo "<td>" . $customerRow["user_first_name"] . " " . $customerRow["user_last_name"]  . "</td>";
+                                    echo "<td>" . $customerRow["user_email"] . "</td>";
+                                    echo "<td>" . $customerRow["user_phone_no"] . "</td>";
+                                    echo "<td>" . $customerRow["current_balance"] . "</td>";
+                                    echo "<td style='display: flex; justify-content: center;'>";
+                                    echo "<a href='#' onclick='openCustomerModal()'>";
+                                    echo "<div><span>View Data</span></div>";
+                                    echo "<div><img src='images/external-link-alt-solid.png' alt='' style='width: 20px; height: 20px;'></div>";
+                                    echo "</a>";
+                                    echo "</td>";
+                                    echo "</tr>";
+                                }
+                            }
+
+                            else {
+                                echo "No Results Found";
+                            }
+                        ?>
                     </tbody>
                 </table>
             </div>
