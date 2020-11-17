a0bc1ecac31987624cda76e7f85c8f367fbce010
Parth Kalbag <10bparth@gmail.com>
2020-11-17 17:11:29 +0530

    Modified jQuery Link Syntax and jQuery available locally

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/index.css">
     <link rel="icon" href="images/favicon.ico" type="image/ico">
     <!-- jQuery CDN -->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
     <title>The Sparks Foundation Bank</title>
 </head>
 <body>
--- transaction_history.php
+++ transaction_history.php
@@ -9,7 +9,7 @@
     <!-- Icon for title -->
     <link rel="icon" href="images/favicon.ico" type="image/ico">
     <!-- jQuery CDN -->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
     <title>Transaction History</title>
 </head>
 <body>
