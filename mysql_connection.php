9a2ecfa674f1b140fdc5fecae00d74296f5af8aa
Parth Kalbag <10bparth@gmail.com>
2020-11-18 15:36:26 +0530

    Added MySQL connection

--- mysql_connection.php
+++ mysql_connection.php
@@ -1,9 +1,9 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "sparksfoundation";
+    $servername = "sql12.freemysqlhosting.net";
+    $username = "sql12377302";
+    $password = "a6v8hF7mGM";
+    $dbname = "sql12377302";
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname, $port=3307);
+    $conn = new mysqli($servername, $username, $password, $dbname, $port=3306);
 ?>
